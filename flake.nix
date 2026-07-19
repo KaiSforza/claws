2313f7b1e7db7da628eff0b278f2e38d217b6432
Kai Giokas <kai@giok.as>
2026-07-19 10:13:22 +0400

    Add self package to flake.nix, nix fixes, fmt
    * Added a `default`/`clawscli` package that builds this project. Note,
      `vendorHash` has to be set when the dependencies change.
    * Uses the same flags as in the `.goreleaser.yml`
    * Uses `self` for automatic version options using the hash of the current
      git revision. Uses the `dirtyShortRev` if it is available so that
      builds and versions show that there are changes to the working copy.
      Nix already takes care of making the output different, so no bumping
      versions are necessary to do `nix build ...`
    * Formatted the flake.nix using `nixfmt`.
    * Used built-in `stdenv.targetPlatform.node` variables for fetching
      indexion, set `meta.platforms` to limit where it can run.
    * Added nix info to the README.

--- flake.nix
+++ flake.nix
@@ -6,58 +6,113 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         lib = pkgs.lib;
 
-        indexionPlatform = {
-          "aarch64-darwin" = "darwin-arm64";
-          "x86_64-darwin" = "darwin-x64";
-          "aarch64-linux" = "linux-arm64";
-          "x86_64-linux" = "linux-x64";
-        }.${system} or (throw "indexion: unsupported system ${system}");
-
-        indexionHash = {
-          "darwin-arm64" = "1w3m4px4f5hjr0776rxf3682rizz909zrvsh8i115ig67n1riqq5";
-          "darwin-x64" = "0000000000000000000000000000000000000000000000000000";
-          "linux-arm64" = "0000000000000000000000000000000000000000000000000000";
-          "linux-x64" = "0y7dbkrxyr56kdz326hxpm6i3pljp2z4jpzv1mc5qqwrzqfgybbd";
-        }.${indexionPlatform};
-
-        # No upstream release binary for this platform yet (e.g. linux-arm64).
-        # Dummy hash = unsupported; drop indexion from the shell instead of failing.
-        indexionSupported = indexionHash != "0000000000000000000000000000000000000000000000000000";
-
-        indexion = pkgs.stdenvNoCC.mkDerivation {
-          pname = "indexion";
-          version = "0.16.0";
-          src = pkgs.fetchzip {
-            url = "https://github.com/trkbt10/indexion/releases/download/v0.16.0/indexion-${indexionPlatform}.tar.gz";
-            sha256 = indexionHash;
-            stripRoot = true;
-          };
-          installPhase = ''
-            mkdir -p $out/bin $out/share/indexion
-            cp indexion $out/bin/
-            cp -r kgfs $out/share/indexion/
-          '';
+        indexion =
+          let
+            indexionHashes = {
+              "aarch64-darwin" = "1w3m4px4f5hjr0776rxf3682rizz909zrvsh8i115ig67n1riqq5";
+              "x86_64-linux" = "0y7dbkrxyr56kdz326hxpm6i3pljp2z4jpzv1mc5qqwrzqfgybbd";
+            };
+            nodePlatform = pkgs.stdenv.targetPlatform.node;
+            indexionFile = "indexion-${nodePlatform.platform}-${nodePlatform.arch}.tar.gz";
+          in
+          pkgs.stdenvNoCC.mkDerivation (fa: {
+            pname = "indexion";
+            version = "0.16.0";
+            src = pkgs.fetchzip {
+              url = "https://github.com/trkbt10/${fa.pname}/releases/download/v${fa.version}/${indexionFile}";
+              sha256 = indexionHashes.${system} or (throw "indexion: unsupported system");
+              stripRoot = true;
+            };
+            installPhase = ''
+              mkdir -p $out/bin $out/share/indexion
+              cp indexion $out/bin/
+              cp -r kgfs $out/share/indexion/
+            '';
+            meta = {
+              platforms = builtins.attrNames indexionHashes;
+            };
+          });
+      in
+      {
+        packages = {
+          clawscli = pkgs.buildGoModule (fa: {
+            pname = "claws";
+            version = "git-${self.dirtyShortRev or self.shortRev}";
+            vendorHash = "sha256-Ef/2Xs15E5noUYJk2J9k48g0kfTPB6v+D9uUHdOyya0=";
+            src =
+              let
+                inherit (pkgs.lib.fileset) toSource unions;
+              in
+              toSource {
+                root = ./.;
+                fileset = unions [
+                  ./go.mod
+                  ./go.sum
+                  ./README.md
+                  ./cmd
+                  ./custom
+                  ./docs
+                  ./internal
+                  ./scripts
+                  ./testdata
+                ];
+              };
+            ldflags = [
+              "-s"
+              "-w"
+              "-X main.version=${fa.version}"
+            ];
+            checkFlags =
+              let
+                # Skip these tests (full names or regexes)
+                tests = [
+                  # Uses absolute paths for checks (/bin/echo, /bin/sh, etc.)
+                  "TestSimpleExecArgsTreatShellMetacharactersAsLiteral"
+                  "TestBuildExecCommandSharedPath"
+                  "TestExecWithHeaderCommandExpandsResourceVariables"
+                  "TestBuildExecCommandSmoke"
+                  "TestResolveArgsExecutableReturnsCopy"
+                  "TestExpandArgsTreatsMetacharactersAsLiteralValues"
+                  "TestProductionExecTemplatesExpand"
+                  "TestExecuteWithDAO_ExecType"
+                  # Check tilde expansion, but build env has /homeless-shelter (ro)
+                  "TestSetConfigPath_TildeExpansion"
+                ];
+              in
+              [
+                "-skip=^${builtins.concatStringsSep "$|^" tests}$"
+              ];
+          });
+          default = self.packages.${system}.clawscli;
         };
-      in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            go_1_25
-            go-task
-            gopls
-            golangci-lint
-            vhs
-            ttyd
-            nodejs
-            bash
-          ] ++ lib.optionals indexionSupported [
-            indexion
-          ];
+          packages =
+            with pkgs;
+            [
+              go_1_25
+              go-task
+              gopls
+              golangci-lint
+              vhs
+              ttyd
+              nodejs
+              bash
+            ]
+            ++ lib.optionals (builtins.elem pkgs.stdenv.targetPlatform.system indexion.meta.platforms) [
+              indexion
+            ];
 
           env.GOROOT = "${pkgs.go_1_25}/share/go";
 
